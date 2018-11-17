e1ddbc06833af13386d7547c9d409b64fc8c9a93
Subhajit Mondal <subhajit25mondal@gmail.com>
2018-11-17 17:06:07 +0530

    yo

--- signup.php
+++ signup.php
@@ -28,26 +28,26 @@
           <div class="container subtag3">
                 <center>Ready to boost your Business? <br> Get noticed by pulling your website to the Search Engine.</center>
 <!--form start-->
-                <form>
+                <form method="POST" action="signup.php">
                         <div class="form-group">
                           <label for="exampleInputEmail1">Enter Email address</label>
-                          <input type="email" class="form-control" placeholder="Enter email">
+                          <input type="email" class="form-control" placeholder="Enter email" name="email">
                           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
                                 <label for="exampleInputPassword1">Full Name</label>
-                                <input type="text" class="form-control" placeholder="Full Name">
+                                <input type="text" class="form-control" placeholder="Full Name" name="name">
                               </div>
                         <div class="form-group">
                           <label for="exampleInputPassword1">Password</label>
-                          <input type="password" class="form-control" placeholder="Password">
+                          <input type="password" class="form-control" placeholder="Password" name="pass1">
                         </div>
                         <div class="form-group">
                                 <label for="exampleInputPassword1">Confirm Password</label>
-                                <input type="password" class="form-control" placeholder="Confirm Password">
+                                <input type="password" class="form-control" placeholder="Confirm Password" name="pass2">
                         </div>
                         <br>
-                        <center><button type="submit" class="btn btn-dark btn-lg">Create an Account</button></center>
+                        <center><button type="submit" name="submit" class="btn btn-dark btn-lg">Create an Account</button></center>
                       </form>
 
 <!--form end-->
@@ -57,3 +57,32 @@
         
     </body>
     </html>
+
+    <?php 
+    
+    if(isset($_POST['submit'])){
+    
+      $email = $_POST['email'];
+      $name = $_POST['name'];
+      $pass1 = $_POST['pass1'];
+      $pass2 = $_POST['pass2'];
+
+      if ($pass1 !== $pass2){
+        echo "<script>alert('Passwords do not match')</script>";
+      }
+      elseif($email=='' || $name==''){
+        echo "<script>alert('Either Name or Email field is empty')</script>";
+      }
+      else {
+        $con = mysqli_connect("localhost","root","");
+        mysqli_select_db($con,"gruuve");
+
+        $insert_query = "insert into users (name, email, password) values ('$name','$email','$pass1')";
+        $res=mysqli_query($con, $insert_query);
+        echo "<script>window.location.href = \"registered.html\";</script>";
+      }
+  
+    
+    }
+    
+    ?>
